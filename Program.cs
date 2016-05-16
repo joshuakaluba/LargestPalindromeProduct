95c778375c67e66305bf2b5c56f022f35fc7b957
Joshua Kaluba <kalubaj@icloud.com>
2016-05-16 14:42:13 -0400

    Added function to determine if a number is a palidndrome

--- Program.cs
+++ Program.cs
@@ -11,5 +11,23 @@ namespace LargestPalindromeProduct
         static void Main(string[] args)
         {
         }
+
+        public static bool isPalindrome(int Number)
+        {
+            bool result = false;
+
+            String res = Number.ToString();
+
+            char[] charArray = res.ToCharArray();
+            Array.Reverse(charArray);
+            String resReversed = new string(charArray);
+
+            if (res.Equals(resReversed))
+            {
+                result = true;
+            }
+
+            return result;
+        }
     }
 }
