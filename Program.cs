2ff0bc848c8b47af149e0cd6befa6f9ff6aeba5d
Joshua Kaluba <kalubaj@icloud.com>
2016-05-16 14:52:08 -0400

    Modified read me

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+/// <summary>
+/// Joshua Kaluba 
+/// 2016-05-16
+/// My take on the problem presented at --> https://projecteuler.net/problem=4
+/// 
+/// Problem states :  
+/// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+/// Find the largest palindrome made from the product of two 3-digit numbers.
+/// </summary>
 
 namespace LargestPalindromeProduct
 {
@@ -10,24 +16,47 @@ namespace LargestPalindromeProduct
     {
         static void Main(string[] args)
         {
-        }
+            int LargestPalindrome = 0, FirstFactor = 0, SecondFactor = 0;
 
-        public static bool isPalindrome(int Number)
-        {
-            bool result = false;
-
-            String res = Number.ToString();
-
-            char[] charArray = res.ToCharArray();
-            Array.Reverse(charArray);
-            String resReversed = new string(charArray);
-
-            if (res.Equals(resReversed))
+            for (int i = 100; i < 1000; i++)
             {
-                result = true;
+                for (int z = 100; z < 1000; z++)
+                {
+                    int Product = i * z;
+
+                    if (Product > LargestPalindrome)
+                    {
+                        if (IsPalindrome(Product))
+                        {
+                            LargestPalindrome = Product;
+                            FirstFactor = i;
+                            SecondFactor = z;
+                        }
+                    }
+
+                }
             }
 
-            return result;
+            Console.WriteLine(FirstFactor.ToString() + " x " + SecondFactor.ToString() + " = " + LargestPalindrome.ToString());
+            Console.Read();
+        }
+
+        public static bool IsPalindrome(int Number)
+        {
+            bool Result = false;
+
+            String Num = Number.ToString();
+
+            char[] CharArray = Num.ToCharArray();
+            Array.Reverse(CharArray);
+            String NumReversed = new string(CharArray);
+
+            if (Num.Equals(NumReversed))
+            {
+                Result = true;
+            }
+
+            return Result;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # LargestPalindromeProduct
+Joshua Kaluba 
+2016-05-16
+My take on the problem presented at --> https://projecteuler.net/problem=4
+ 
+Problem states :  
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+See more of my projects at http://www.joshuakaluba.com
