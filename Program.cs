34e9a6db14d946f7737540c36d7bda7f683fffc5
Joshua Kaluba <joshkaluba@gmail.com>
2017-05-14 11:09:19 -0400

    Updated to .Net 4.7 and general code cleanup

--- Program.cs
+++ Program.cs
@@ -1,84 +1,83 @@
 ﻿using System;
 
 /// <summary>
-/// Joshua Kaluba 
+/// Joshua Kaluba
 /// 2016-05-16
 /// My take on the problem presented at --> https://projecteuler.net/problem=4
-/// 
-/// Problem states :  
+///
+/// Problem states :
 /// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 /// Find the largest palindrome made from the product of two 3-digit numbers.
 /// </summary>
 
 namespace LargestPalindromeProduct
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Variables to hold the palindrome and the palindromes factors
-            int LargestPalindrome = 0, FirstFactor = 0, SecondFactor = 0;
+	internal class Program
+	{
+		private static void Main ( string[] args )
+		{
+			//Variables to hold the palindrome and the palindromes factors
+			int largestPalindrome = 0, firstFactor = 0, secondFactor = 0;
 
-            for (int i = 100; i < 1000; i++)
-            {
-                //Loop for the first factor
+			for ( int i = 100 ; i < 1000 ; i++ )
+			{
+				//Loop for the first factor
 
-                for (int z = 100; z < 1000; z++)
-                {
-                    //Loop for the second factor
+				for ( int z = 100 ; z < 1000 ; z++ )
+				{
+					//Loop for the second factor
 
-                    //Product of the two factors
-                    int Product = i * z;
+					//Product of the two factors
+					int product = i * z;
 
-                    //if the product is greater than the last palindromic number that was found
-                    if (Product > LargestPalindrome)
-                    {
-                        //and the product is a palindrome
-                        if (IsPalindrome(Product))
-                        {
-                            //assign the product and its factors to the variables
-                            LargestPalindrome = Product;
-                            FirstFactor = i;
-                            SecondFactor = z;
-                        }
-                    }
+					//if the product is greater than the last palindromic number that was found
+					if ( product > largestPalindrome )
+					{
+						//and the product is a palindrome
+						if ( IsPalindrome( product.ToString() ))
+						{
+							//assign the product and its factors to the variables
+							largestPalindrome = product;
+							firstFactor = i;
+							secondFactor = z;
+						}
+					}
+				}
+			}
 
-                }
-            }
+			//output
+			Console.WriteLine( String.Format("{0} * {1} = {2}", 
+									firstFactor.ToString(), 
+										secondFactor.ToString(), 
+											largestPalindrome.ToString() ) );
 
-            //output
-            Console.WriteLine(FirstFactor.ToString() + " x " + SecondFactor.ToString() + " = " + LargestPalindrome.ToString());            
-            Console.Read();
+			Console.Read();
 
+			//Answer = 913 x 993 = 906609
+		}
 
-            //Answer = 913 x 993 = 906609
-        }
+		public static bool IsPalindrome ( string number )
+		{
+			//Determines if a number passed is a palindromic number
 
-        public static bool IsPalindrome(int Number)
-        {
-            //Determines if a number passed is a palindromic number
+			bool result = false;
 
-            bool Result = false;
+			//convert string to char array
+			char[ ] charArray = number.ToCharArray();
 
-            //convert number to string
-            String Num = Number.ToString();
+			//reverse the char array
+			Array.Reverse( charArray );
 
-            //convert string to char array
-            char[] CharArray = Num.ToCharArray();
+			//declare a new string from the reversed char array
+			String numberReversed = new string( charArray );
 
-            //reverse the char array
-            Array.Reverse(CharArray);
+			//if the original number is equal to the reversed number, it is a palindrome
+			if ( number.Equals( numberReversed ) )
+			{
+				result = true;
+			}
 
-            //declare a new string from the reversed char array
-            String NumReversed = new string(CharArray);
-
-            //if the original number is equal to the reversed number, it is a palindrome
-            if (Num.Equals(NumReversed))
-            {
-                Result = true;
-            }
-
-            return Result;
-        }
-    }
+			return result;
+		}
+	}
 }
